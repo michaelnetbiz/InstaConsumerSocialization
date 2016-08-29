93dac56843d640d354e94c4208fd725753281192
Michael Nelson <michael.nelson@fulbrightmail.org>
2016-08-29 19:46:14 -0400

    add Stimulus model, gridview for display

---
+++ InstaConsumerSocialization/MainPageData.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstaConsumerSocialization
+{
+    public class MainPageData : INotifyPropertyChanged
+    {
+        StreamWriter log;
+        private List<Stimulus> _allStimuli = new List<Stimulus>();
+        public ObservableCollection<Stimulus> Stimuli { get; set; }
+        public MainPageData()
+        {
+            Stimuli = new ObservableCollection<Stimulus>();
+            for (int stimulusId = 1; stimulusId <= 20; stimulusId++)
+            {
+                _allStimuli.Add(new Stimulus(
+                    stimulusId,
+                    "Stimulus Name For Stimulus No. " + stimulusId,
+                    "Stimulus Caption For Stimulus No. " + stimulusId,
+                    "Assets/stimuli/stimulus-" + stimulusId + ".png",
+                    "Assets/stimuli/stimulus-" + stimulusId + ".png",
+                    new DateTime(),
+                    stimulusId,
+                    new List<String>(),
+                    false,
+                    stimulusId,
+                    stimulusId
+                    ));
+            }
+            PerformFiltering();
+        }
+
+        private void PerformFiltering()
+        {
+            if (_filter == null)
+            {
+                _filter = "";
+            }
+            var lowerCaseFilter = Filter.ToLowerInvariant().Trim();
+            var result = _allStimuli.Where(d => d.StimuliNamesAsString.ToLowerInvariant().Contains(lowerCaseFilter)).ToList();
+            var toRemove = Stimuli.Except(result).ToList();
+            foreach (var x in toRemove)
+                Stimuli.Remove(x);
+            var resultCount = result.Count;
+            for (int i = 0; i < resultCount; i++)
+            {
+                var resultItem = result[i];
+                if (i + 1 > Stimuli.Count || !Stimuli[i].Equals(resultItem))
+                {
+                    Stimuli.Insert(i, resultItem);
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private Stimulus _selectedStimulus;
+        public Stimulus SelectedStimulus
+        {
+            get { return _selectedStimulus; }
+            set
+            {
+                _selectedStimulus = value;
+                if (value == null)
+                    log.WriteLine(true);
+                else
+                    log.WriteLine(false);
+                    
+            }
+        }
+        private string _filter;
+        public string Filter
+        {
+            get { return _filter; }
+            set
+            {
+                if (value == _filter)
+                    return;
+                _filter = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Filter)));
+
+                PerformFiltering();
+            }
+        }
+
+    }
+}
---
+++ InstaConsumerSocialization/Stimulus.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InstaConsumerSocialization
+{
+    public class Stimulus
+    {
+        public int StimulusId { get; }
+        public string StimulusName { get; }
+        public string StimulusCaption { get; }
+        public string StimulusImagePath { get; }
+        public string StimulusAuthorProfileImagePath { get; }
+        public DateTime StimulusDateTime { get; }
+        public int StimulusAuthorId { get; }
+        public IEnumerable<string> StimulusResponses { get; set; }
+        public bool StimulusIsFavoritedByParticipant { get; set; }
+        public int NumberOfFavorites { get; set; }
+        public int NumberOfResponses { get; set; }
+        public Stimulus(
+            int stimulusId,
+            string stimulusName,
+            string stimulusCaption,
+            string stimulusImagePath,
+            string stimulusAuthorProfileImagePath,
+            DateTime stimulusDateTime,
+            int stimulusAuthorId,
+            IEnumerable<string> stimulusResponses,
+            bool stimulusIsFavoritedByParticipant,
+            int numberOfFavorites,
+            int numberOfResponses
+            )
+        {
+            StimulusId = stimulusId;
+            StimulusName = stimulusName;
+            StimulusCaption = stimulusCaption;
+            StimulusImagePath = stimulusImagePath;
+            StimulusAuthorProfileImagePath = stimulusAuthorProfileImagePath;
+            StimulusDateTime = stimulusDateTime;
+            StimulusAuthorId = stimulusAuthorId;
+            StimulusResponses = stimulusResponses;
+            StimulusIsFavoritedByParticipant = stimulusIsFavoritedByParticipant;
+            NumberOfFavorites = numberOfFavorites;
+            NumberOfResponses = numberOfResponses;
+        }
+
+        public string StimuliNamesAsString => string.Join(", ", StimulusName);
+    }
+}
