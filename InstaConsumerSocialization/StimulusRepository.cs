e6bd530bd27aba072c0c8e7e32fdcc2b103f0426
Michael Nelson <michael.nelson@fulbrightmail.org>
2016-08-30 13:09:11 -0400

    add StimulusRepository

---
+++ InstaConsumerSocialization/StimulusRepository.cs
@@ -0,0 +1,24 @@
+﻿using InstaConsumerSocialization.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace InstaConsumerSocialization
+{
+    public static class StimulusRepository
+    {
+        private static List<Stimulus> allStimuliCache;
+        public static async Task<List<Stimulus>> GetAllStimuliAsync()
+        {
+            if (allStimuliCache != null)
+                return allStimuliCache;
+
+            var client = new HttpClient();
+            var stream = await client.GetStreamAsync("https://msu.edu/~nelso663/assets/posts.json");
+            var serializer = new DataContractJsonSerializer(typeof(List<Stimulus>));
+            return allStimuliCache;
+        }
+    }
+
+}
