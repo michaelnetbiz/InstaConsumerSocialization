7d7ca8f25e625eb5a9f637d78321e7a80475c717
Michael Nelson <michael.nelson@fulbrightmail.org>
2016-10-14 13:42:02 -0400

    update Post data model and repoitory

--- InstaConsumerSocialization/Models/Post.cs
+++ InstaConsumerSocialization/Models/Post.cs
@@ -4,104 +4,30 @@ using System.Linq;
 using System.Runtime.Serialization;
 
 namespace InstaConsumerSocialization.Models
-{
-    [DataContract]
+{   [DataContract]
     public class Post
     {
         [DataMember]
-        public string Caption { get; set; }
-
-        [DataMember]
-        public List<string> Comments { get; set; }
-
-        [DataMember]
-        public DateTime CreatedTime { get; set; }
-
-        [DataMember]
-        public string Filter { get; set; }
-
-        [DataMember]
         public string Id { get; set; }
-
         [DataMember]
-        public int Index { get; set; }
-
+        public int CreationDay { get; set; }
         [DataMember]
-        public string Image { get; set; }
-
+        public int CreationMonth { get; set; }
         [DataMember]
-        public List<string> Likes { get; set; }
-
-        /// <summary>
-        /// Gets/sets post's location
-        /// </summary>
-        /// <value>
-        /// Post location.
-        /// </value>
+        public int CreationYear { get; set; }
         [DataMember]
-        public decimal Lat { get; set; }
-
+        public string Author { get; set; }
         [DataMember]
-        public decimal Lng { get; set; }
-
-        public IEnumerable<string> Names { get; set; }
-
+        public string Caption { get; set; }
         [DataMember]
-        public List<string> Tags { get; set; }
-
+        public int LikesNumber { get; set; }
         [DataMember]
-        public string Type { get; set; }
-
+        public int CommentsNumber { get; set; }
         [DataMember]
-        public string Url { get; set; }
+        public Uri ImageUri { get; set; }
 
-        [DataMember]
-        public string User { get; set; }
-
-        [DataMember]
-        public bool ViewerLikedPost { get; set; }
-
-        public Post(
-            string captionText,
-            List<string> comments,
-            DateTime createdTime,
-            string filter,
-            string id,
-            int index,
-            string imagePath,
-            List<string> likes,
-            decimal lat,
-            decimal lng,
-            IEnumerable<string> names,
-            List<string> tags,
-            string type,
-            string url,
-            string user,
-            bool viewerLikedPost
-        )
-        {
-            Caption = captionText;
-            Comments = comments;
-            CreatedTime = createdTime;
-            Filter = filter;
-            Id = id;
-            Index = index;
-            Image = imagePath;
-            Likes = likes;
-            Lat = lat;
-            Lng = lng;
-            Names = names;
-            Tags = tags;
-            Type = type;
-            Url = url;
-            User = user;
-            ViewerLikedPost = viewerLikedPost;
-        }
-
-        public Post() { }
-
-        public string NamesAsString => string.Join(", ", new []
-        {
+        public string NamesAsString => string.Join(", ", new[]
+{
             "lol"
         });
     }
--- InstaConsumerSocialization/PostRepository.cs
+++ InstaConsumerSocialization/PostRepository.cs
@@ -11,40 +11,17 @@ namespace InstaConsumerSocialization
 {
     public static class PostRepository
     {
-        public static List<Post> AllPostsCache { get; private set; }
+        public static List<Post> allPostsCache { get; private set; }
 
         public static async Task<List<Post>> GetAllStimuliAsync()
         {
-            if (AllPostsCache != null)
-                return AllPostsCache;
-            HttpClient http = new HttpClient();
-            http.DefaultRequestHeaders.Add("accept", "Application/JSON");
-            string result = await http.GetStringAsync("http://beta.json-generator.com/api/json/get/N14ZKoR9b");
-            //var stream = await client.GetStreamAsync("http://beta.json-generator.com/api/json/get/N14ZKoR9b");
-            //var firstDegreeTypes = new[]
-            //{
-            //    typeof(List<Post>),
-            //    typeof(Post)
-            //};
-            var secondDegreeTypes = new[]
-            {
-                typeof(DateTime),
-                typeof(Caption),
-                typeof(Comment),
-                typeof(Identifier),
-                typeof(Image),
-                typeof(Incrementer),
-                typeof(Like),
-                typeof(Location),
-                typeof(Tag),
-                typeof(User),
-                typeof(UserInfo)
-            };
-            //List<Post> posts = new JavaScriptSerializer().Deserialize<Posts>(stream);
-            AllPostsCache = JsonConvert.DeserializeObject<List<Post>>(result);
-            //var serializer = new DataContractJsonSerializer(typeof(List<Post>), secondDegreeTypes);
-            //AllPostsCache = (List<Post>)serializer.ReadObject(stream);
-            return AllPostsCache;
+            if (allPostsCache != null)
+                return allPostsCache;
+            var httpClient = new HttpClient();
+            var stream = await httpClient.GetStreamAsync("http://icswebservice20161010034859.azurewebsites.net/api/posts");
+            var serializer = new DataContractJsonSerializer(typeof(List<Post>));
+            allPostsCache = (List<Post>)serializer.ReadObject(stream);
+            return allPostsCache;
         }
     }
 
