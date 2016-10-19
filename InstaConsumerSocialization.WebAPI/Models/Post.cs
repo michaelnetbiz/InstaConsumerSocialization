f839a4b06196f6faad3de3112ed3754e13a871eb
Michael Nelson <michael.nelson@fulbrightmail.org>
2016-10-19 13:16:05 -0400

    add Post model

---
+++ InstaConsumerSocialization.WebAPI/Models/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InstaConsumerSocialization.WebAPI.Models
+{
+    public class Post
+    {
+        public string Id { get; set; }
+        public int CreationDay { get; set; }
+        public int CreationMonth { get; set; }
+        public int CreationYear { get; set; }
+        public string Author { get; set; }
+        public Uri AuthorImageUri { get; set; }
+        public string Caption { get; set; }
+        public bool IsLikedByUser { get; set; }
+        public int LikesNumber { get; set; }
+        public int CommentsNumber { get; set; }
+        public Uri PostImageUri { get; set; }
+        public string TimeSince => string.Concat(Math.Round((Decimal)TimeLOL.TotalDays, 0, MidpointRounding.AwayFromZero), "d");
+    }
+}
