e1bf797e1cd6d0fc1bed7e244b8d30cda2119841
Michael Nelson <michael.nelson@fulbrightmail.org>
2016-10-19 10:35:22 -0400

    add ShufflePosts method

--- InstaConsumerSocialization/PostRepository.cs
+++ InstaConsumerSocialization/PostRepository.cs
@@ -11,6 +11,28 @@ namespace InstaConsumerSocialization
 {
     public static class PostRepository
     {
+        private static Random rng = new Random();
+        public static void ShufflePosts()
+        {
+            int n = allPostsCache.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                var value = allPostsCache[k];
+                allPostsCache[k] = allPostsCache[n];
+                allPostsCache[n] = value;
+            }
+        }
+
+        //public static async void postSequenceToServer()
+        //{
+        //    var httpClient = new HttpClient();
+        //    var lol = allPostsCache;
+        //    var postingStream = await httpClient.PostAsync("http://icswebservice20161010034859.azurewebsites.net/api/posts", lol);
+        //    var postingSerializer = new DataContractJsonSerializer(typeof(List<Post>));
+        //}
+
         public static List<Post> allPostsCache { get; private set; }
 
         public static async Task<List<Post>> GetAllPostsAsync()
@@ -21,6 +43,8 @@ namespace InstaConsumerSocialization
             var stream = await httpClient.GetStreamAsync("http://icswebservice20161010034859.azurewebsites.net/api/posts");
             var serializer = new DataContractJsonSerializer(typeof(List<Post>));
             allPostsCache = (List<Post>)serializer.ReadObject(stream);
+            ShufflePosts();
+            //postSequenceToServer();
             return allPostsCache;
         }
     }
