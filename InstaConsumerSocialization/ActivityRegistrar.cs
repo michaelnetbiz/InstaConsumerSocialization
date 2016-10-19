d2e969673918814fdf182bfae970e258836a55a3
Michael Nelson <michael.nelson@fulbrightmail.org>
2016-10-19 10:40:56 -0400

    add class ActivityRegistrar

---
+++ InstaConsumerSocialization/ActivityRegistrar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+
+namespace InstaConsumerSocialization
+{
+    class ActivityRegistrar
+    {
+        public ActivityRegistrar()
+        {
+
+        }
+        public void RegisterLike()
+        {
+            var httpClient = new HttpClient();
+        }
+    }
+}
